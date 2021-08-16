9bca490be027d1ea8977fcb962e1bcefe1699488
rohitsainik <rsainik.s.woodenstreet@gmail>
2021-08-16 10:40:04 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/getxwithlocalauth/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.getxwithlocalauth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.orange,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: SvgPicture.asset('assets/new.svg',fit: BoxFit.cover,width: MediaQuery.of(context).size.width,),
+      ),
+    );
+  }
+}
